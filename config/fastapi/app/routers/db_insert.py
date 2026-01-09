d8cf4bc5f7ed314e1b72b24bd01c11de0632cca1
Michal-IO <u56169@student.wat.edu.pl>
2026-01-09 12:26:42 +0100

    fix

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,7 +1,7 @@
 from urllib import request
 
 from fastapi import APIRouter
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 from app.settings import db_name, db_user, db_password
 
@@ -10,7 +10,7 @@ router_insert = APIRouter()
 
 def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
-        f'postgresql://{db_user}:{db_password}@postgis:5432/{db_name}',
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}",
     )
 
 
@@ -20,15 +20,15 @@ async def insert_user():
         db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
         params = {
-            'name': request.name,
-            'posts': request.posts,
-            'location': request.location,
+            "name": request.name,
+            "posts": request.posts,
+            "location": request.location,
         }
 
         sql_query = text("""
-                    insert into users(name, posts, location)
-                    values (:name, :posts, :location); \
-                    """)
+                         insert into users (name, posts, location)
+                         values (:name, :posts, :location); \
+                         """)
 
         with db_connection.connect() as connection:
             result = connection.execute(sql_query, params)
