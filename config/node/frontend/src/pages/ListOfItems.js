a5a73a8e90c5b200e3cecd41f66754f766137345
Michal-IO <u56169@student.wat.edu.pl>
2026-02-04 01:39:34 +0100

    feat: frontend development

--- config/node/frontend/src/pages/ListOfItems.js
+++ config/node/frontend/src/pages/ListOfItems.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import UserCard from "../components/UserCard";
 import {
+    Box,
+    Button,
     Container,
     Paper, Stack,
     Table,
@@ -15,10 +17,12 @@ import {
 import FlightIcon from '@mui/icons-material/Flight';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import TagIcon from '@mui/icons-material/Tag';
+import {useNavigate} from "react-router-dom";
 
 function ListOfItems(props) {
 
     const [users, setUsers] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -35,28 +39,34 @@ function ListOfItems(props) {
 
     return (
         <Container maxWidth="lg" sx={{mt: 4}}>
-            <Typography variant="h5" sx={{mb: 4, fontWeight: 'bold', color: 'white'}}>
-                Lista Dodanych Lotnisk
-            </Typography>
+            <Stack direction="row" sx={{mb: 2, width: '100%'}} alignItems="center">
+                <Typography variant="h5" sx={{fontWeight: 'bold', color: 'white'}}>
+                    Lista Dodanych Lotnisk
+                </Typography>
+                <Box sx={{flexGrow: 1}}/>
+                <Button variant="contained" onClick={() => navigate('/services')}
+                        sx={{backgroundColor: 'white', color: 'blue', marginLeft: 'auto'}}>Powrót</Button>
+            </Stack>
+
             <Paper sx={{width: '100%', overflow: 'hidden', borderRadius: 4, boxShadow: 5}}>
                 <TableContainer sx={{maxHeight: 600}}>
                     <Table stickyHeader aria-label="sticky table">
                         <TableHead>
                             <TableRow>
                                 <TableCell sx={{textAlign: 'center', backgroundColor: '#f5f5f5'}}>
-                                    <Stack direction="row" alignItems="center" spacing={1}>
+                                    <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
                                         <FlightIcon color="primary"/>
                                         <Typography>Nazwa Lotniska</Typography>
                                     </Stack>
                                 </TableCell>
                                 <TableCell sx={{textAlign: 'center', backgroundColor: '#f5f5f5'}}>
-                                    <Stack direction="row" alignItems="center" spacing={1}>
+                                    <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
                                         <LocationOnIcon color="primary"/>
                                         <Typography>Miasto</Typography>
                                     </Stack>
                                 </TableCell>
                                 <TableCell sx={{textAlign: 'center', backgroundColor: '#f5f5f5'}}>
-                                    <Stack direction="row" alignItems="center" spacing={1}>
+                                    <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
                                         <TagIcon color="primary"/>
                                         <Typography>Kod IATA</Typography>
                                     </Stack>
